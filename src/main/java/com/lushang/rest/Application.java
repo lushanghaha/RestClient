2debcab5f110fccb99485d67be8d13a93da5f1c1
lushanghaha <lushanghaha@hotmail.com>
2016-11-17 15:09:34 +0800

    以 java.net 實作 REST client，目前只支援 GET 跟 POST

--- src/main/java/com/lushang/rest/Application.java
+++ src/main/java/com/lushang/rest/Application.java
@@ -6,63 +6,54 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.Header;
-import org.apache.http.message.BasicHeader;
-
-import com.lushang.rest.client.RestClient;
+import com.lushang.rest.client.Header;
 import com.lushang.rest.client.RestRequest;
 import com.lushang.rest.client.RestRequest.Method;
-import com.lushang.rest.client.RestResponse;
 
 public class Application {
 	public static void main (String[] args) {
 
 		// 設定 main function 裡面要測試的 REST request 之 method
-		Method methodForTest = Method.GET;
+		Method methodForTest = Method.POST;
 		
 		// LuShang's RESTful server
 		String url = "http://localhost:8080/country-rest-server/countries";
 		List<Header> headers = new ArrayList<Header>();
 		String body = "";	
-		RestClient client = new RestClient();
 		RestRequest request = null;
-		RestResponse response = null;
-		BufferedReader br = null;
 		
 		// 使用方式 (依據不同 methodForTest 決定)
 		switch (methodForTest) {
 			case GET:
-				headers.add(new BasicHeader("accept", "application/json"));
+				headers.add(new Header("Accept", "application/json"));
 				request = new RestRequest(url, Method.GET, headers, body);
 				break;
 			case POST:
-				headers.add(new BasicHeader("accept", "application/json"));
-				headers.add(new BasicHeader("content-type", "application/json"));
+				// headers.add(new Header("Accept", "application/json"));
+				headers.add(new Header("Content-Type", "application/json"));
 				body = "{\"countryName\":\"DoCCgCc\",\"population\":77770}";
 				request = new RestRequest(url, Method.POST, headers, body);
 				break;
 			case PUT:
-				headers.add(new BasicHeader("accept", "application/json"));
-				headers.add(new BasicHeader("content-type", "application/json"));
+				// headers.add(new Header("Accept", "application/json"));
+				headers.add(new Header("Content-Type", "application/json"));
 				body = "{\"id\":1,\"countryName\":\"CHT\",\"population\":100000}";
 				request = new RestRequest(url, Method.PUT, headers, body);
 				break;
 			case DELETE:
 				url = "http://localhost:8080/country-rest-server/country/2";
-				headers.add(new BasicHeader("accept", "application/json"));
+				headers.add(new Header("Accept", "application/json"));
 				request = new RestRequest(url, Method.DELETE, headers, body);
 				break;
 			default:
 				break;
 		}
 		
-		// 使用方式
-		response = client.execute(request);
 		try {
-			br = new BufferedReader(new InputStreamReader((response.getEntityInputStream())));
+			BufferedReader br = new BufferedReader(new InputStreamReader((request.getInputStream())));
 			// 輸出結果
 			String output;
-			System.out.println("HTTP status code: " + response.getHttpStatusCode());
+			System.out.println("HTTP status code: " + request.getResponseCode());
 			System.out.println("Output from Server:");
 			try {
 				while ((output = br.readLine()) != null) {
@@ -77,6 +68,6 @@ public class Application {
 		}
 		
 		// 使用方式
-		client.close();
+		request.disconnect();
 	}
 }
--- src/main/java/com/lushang/rest/client/RestRequest.java
+++ src/main/java/com/lushang/rest/client/RestRequest.java
@@ -1,95 +1,108 @@
 package com.lushang.rest.client;
 
-import java.io.UnsupportedEncodingException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
-
-import org.apache.http.Header;
-import org.apache.http.HttpRequest;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPatch;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.entity.StringEntity;
 
 
 public class RestRequest {
-
-	// HTTP requests
-	private HttpGet getRequest;
-	private HttpPost postRequest;
-	private HttpPut putRequest;
-	private HttpDelete deleteRequest;
-	private HttpPatch patchRequest;
 	
-	// 此 request 的 method
-	private Method method;
+	private HttpURLConnection conn;
 	
 	// 參數化的 REST request
 	public RestRequest(String url, Method method, List<Header> headers, String body) {		
 		
-		// 提供給含有 HttpEntity 的 HTTP requests HttpEntity，如 PUT, POST, PATCH 等
-		StringEntity input = null;
-		
+		String input = null;
+		OutputStream os = null;
+		URL serverUrl = null;
+	
 		switch (method) {
 			case GET:
-				this.method = Method.GET;
-				getRequest = new HttpGet(url);
+				try {
+					serverUrl = new URL(url);
+					conn = (HttpURLConnection) serverUrl.openConnection();
+					conn.setRequestMethod(method.toString());
+					// 將所有 HTTP requests 加入 headers
+					for (Header header : headers) {
+						conn.setRequestProperty(header.getKey(), header.getValue());
+					}
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				break;
 			case POST:
-				this.method = Method.POST;
-				postRequest = new HttpPost(url);
 				try {
-					input = new StringEntity(body);
-					postRequest.setEntity(input);
-				} catch (UnsupportedEncodingException e) {
+					serverUrl = new URL(url);
+					conn = (HttpURLConnection) serverUrl.openConnection();
+					conn.setDoOutput(true);
+					conn.setRequestMethod(method.toString());
+					// 將所有 HTTP requests 加入 headers
+					for (Header header : headers) {
+						conn.setRequestProperty(header.getKey(), header.getValue());
+					}
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				input = body;
+				try {
+					os = conn.getOutputStream();
+					os.write(input.getBytes());
+					os.flush();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				break;
 			case PUT:
-				this.method = Method.PUT;
-				putRequest = new HttpPut(url);
-				try {
-					input = new StringEntity(body);
-					putRequest.setEntity(input);
-				} catch (UnsupportedEncodingException e) {
-					e.printStackTrace();
-				}
 				break;
 			case DELETE:
-				this.method = Method.DELETE;
-				deleteRequest = new HttpDelete(url);
 				break;
 			case PATCH:
 				break;
 			default:
 				break;
-		}
-		
-		// 將所有 HTTP requests 加入 headers
-		for (Header header : headers) {
-			this.getRequest().addHeader(header.getName(), header.getValue());
-		}
-	}
-	
-	public HttpRequest getRequest() {
-		switch (this.method) {
-			case GET:
-				return getRequest;
-			case POST:
-				return postRequest;
-			case PUT:
-				return putRequest;
-			case DELETE:
-				return deleteRequest;
-			case PATCH:
-				return patchRequest;
-			default:
-				return null;
 		}
 	}
 
 	public enum Method {
 	    GET, POST, PUT, DELETE, PATCH
+	}
+
+	public InputStream getInputStream() {
+		// TODO Auto-generated method stub
+		try {
+			return conn.getInputStream();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public int getResponseCode() {
+		// TODO Auto-generated method stub
+		try {
+			return conn.getResponseCode();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public void disconnect() {
+		conn.disconnect();
 	}
 }
